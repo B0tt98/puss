61e973c8fc220468c2878792d1860f1b8532ef1d
B0tt98 <meow98000@gmail.com>
2024-10-02 21:39:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7296382705:AAFSE1pURVyx4M0Qr9nlssGGPZm6HKG-pF0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("VOFASHWATHAMA")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6965153309")
+
+
+
+
+
+
+
+  
